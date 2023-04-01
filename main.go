c32d23ddb38d9871d8cd152b850a80512e6e6da1
Ryo Kawamura <kawa1214mura@gmail.com>
2023-04-01 09:48:55 +0900

    refactoring: http

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"math/rand"
 	"os"
-	"strings"
 	"syscall"
 	"time"
 	"unsafe"
 
 	"github.com/kawa1214/tcp-ip-go/ip"
+	"github.com/kawa1214/tcp-ip-go/server"
 	"github.com/kawa1214/tcp-ip-go/socket"
 	"github.com/kawa1214/tcp-ip-go/tcp"
 )
@@ -105,7 +104,7 @@ func main() {
 				continue
 			}
 
-			req, err := parseHTTPRequest(string(buf[ipHeader.IHL*4+tcpHeader.DataOff*4:]))
+			req, err := server.ParseHTTPRequest(string(buf[ipHeader.IHL*4+tcpHeader.DataOff*4:]))
 			if err != nil {
 				continue
 			}
@@ -125,10 +124,10 @@ func main() {
 }
 
 func sendAckResponseWithPayload(file *os.File, ipHeader *ip.Header, tcpHeader *tcp.Header, dataLen int) {
-	payload := []byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 15\r\n\r\nHello, World!\r\n")
-	payloadLen := len(payload)
+	response := server.NewTextOkResponse("Hello, World!\r\n")
+	payload := response.String()
 
-	newIPHeader := ip.New(ipHeader.DstIP, ipHeader.SrcIP, tcp.LENGTH+payloadLen)
+	newIPHeader := ip.New(ipHeader.DstIP, ipHeader.SrcIP, tcp.LENGTH+len(payload))
 	ipHeaderPacket := newIPHeader.Marshal()
 	newIPHeader.SetChecksum(ipHeaderPacket)
 	ipHeaderPacket = newIPHeader.Marshal()
@@ -212,57 +211,4 @@ func sendFinAck(file *os.File, ipHeader *ip.Header, tcpHeader *tcp.Header, dataL
 	} else {
 		log.Printf("SYN-ACK packet sent")
 	}
-}
-
-type HTTPRequest struct {
-	Method  string
-	URI     string
-	Version string
-	Headers map[string]string
-	Body    string
-}
-
-func parseHTTPRequest(rawRequest string) (*HTTPRequest, error) {
-	scanner := bufio.NewScanner(strings.NewReader(rawRequest))
-	var requestLine string
-
-	if scanner.Scan() {
-		requestLine = scanner.Text()
-	} else {
-		return nil, fmt.Errorf("failed to read request line")
-	}
-
-	parts := strings.Split(requestLine, " ")
-	if len(parts) != 3 {
-		return nil, fmt.Errorf("invalid request line: %s", requestLine)
-	}
-
-	request := &HTTPRequest{
-		Method:  parts[0],
-		URI:     parts[1],
-		Version: parts[2],
-		Headers: make(map[string]string),
-	}
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		if line == "" {
-			break
-		}
-
-		headerParts := strings.SplitN(line, ": ", 2)
-		if len(headerParts) != 2 {
-			return nil, fmt.Errorf("invalid header: %s", line)
-		}
-
-		request.Headers[headerParts[0]] = headerParts[1]
-	}
-
-	if request.Method == "POST" || request.Method == "PUT" {
-		for scanner.Scan() {
-			request.Body += scanner.Text()
-		}
-	}
-
-	return request, nil
 }
---
+++ server/http.go
@@ -0,0 +1,108 @@
+package server
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"unicode/utf8"
+)
+
+type HTTPRequest struct {
+	Method  string
+	URI     string
+	Version string
+	Headers map[string]string
+	Body    string
+}
+
+type HTTPResponse struct {
+	Version string
+	Status  string
+	Headers map[string]string
+	Body    string
+}
+
+const (
+	HTTP_VERSION = "HTTP/1.0"
+)
+
+// Parse an HTTP request from a string.
+func ParseHTTPRequest(raw string) (*HTTPRequest, error) {
+	scanner := bufio.NewScanner(strings.NewReader(raw))
+	var requestLine string
+
+	if scanner.Scan() {
+		requestLine = scanner.Text()
+	} else {
+		return nil, fmt.Errorf("failed to read request line")
+	}
+
+	parts := strings.Split(requestLine, " ")
+	if len(parts) != 3 {
+		return nil, fmt.Errorf("invalid request line: %s", requestLine)
+	}
+
+	request := &HTTPRequest{
+		Method:  parts[0],
+		URI:     parts[1],
+		Version: parts[2],
+		Headers: make(map[string]string),
+	}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			break
+		}
+
+		headerParts := strings.SplitN(line, ": ", 2)
+		if len(headerParts) != 2 {
+			return nil, fmt.Errorf("invalid header: %s", line)
+		}
+
+		request.Headers[headerParts[0]] = headerParts[1]
+	}
+
+	if request.Method == "POST" || request.Method == "PUT" {
+		for scanner.Scan() {
+			request.Body += scanner.Text()
+		}
+	}
+
+	return request, nil
+}
+
+// Create a new HTTP response.
+func NewTextOkResponse(body string) *HTTPResponse {
+	res := HTTPResponse{
+		Version: HTTP_VERSION,
+		Status:  "200 OK",
+		Headers: map[string]string{
+			"Content-Type":   "text/plain",
+			"Content-Length": fmt.Sprintf("%d", getContentLength(body)),
+		},
+		Body: body,
+	}
+
+	return &res
+}
+
+func getContentLength(body string) int {
+	return utf8.RuneCountInString(body)
+}
+
+// Convert an HTTP response to a string.
+func (res *HTTPResponse) String() string {
+	var response string
+
+	response += fmt.Sprintf("%s %s\r\n", res.Version, res.Status)
+
+	for key, value := range res.Headers {
+		response += fmt.Sprintf("%s: %s\r\n", key, value)
+	}
+	response += "\r\n"
+
+	response += res.Body
+
+	return response
+}
