64e1754f74c96adac125e15733137f60243b8b54
Ryo Kawamura <kawa1214mura@gmail.com>
2023-06-26 22:09:27 +0900

    chore: delete file

--- main.go
+++ main.go
@@ -1,34 +1,5 @@
 package main
 
-import (
-	"log"
-
-	"github.com/kawa1214/tcp-ip-go/application"
-)
-
 func main() {
-	s := application.NewServer()
-	defer s.Close()
-	s.ListenAndServe()
 
-	for {
-		conn, err := s.Accept()
-		if err != nil {
-			log.Printf("accept error: %s", err)
-			continue
-		}
-
-		reqRaw := string(conn.Pkt.Packet.Buf[conn.Pkt.IpHeader.IHL*4+conn.Pkt.TcpHeader.DataOff*4:])
-		req, err := application.ParseHTTPRequest(reqRaw)
-		if err != nil {
-			log.Printf("parse error: %s", err)
-			continue
-		}
-
-		log.Printf("request: %v", req)
-		if req.Method == "GET" && req.URI == "/" {
-			resp := application.NewTextOkResponse("Hello, World!\r\n")
-			s.Write(conn, resp)
-		}
-	}
 }
